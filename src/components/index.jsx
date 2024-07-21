cbdb4b74b6bb9ce6976cd8acc8612df550cbffc3
aryachirag11 <aryachirag11@gmail.com>
2024-07-21 09:09:59 +0530

    Complete project setup with dependencies, folder & file structure, assests

---
+++ src/components/index.jsx
@@ -0,0 +1,15 @@
+export { default as Button } from "./Button";
+export { default as ThemeSetting } from "./ThemeSetting";
+export { default as SideBar } from "./SideBar";
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
+export { default as Cart } from "./Cart";
+export { default as Chat } from "./Chat";
+export { default as Notifications } from "./Notifications";
+export { default as UserProfile } from "./UserProfile";
+export { default as SparkLineChart } from "./Charts/SparkLineChart";
+export { default as AreaChart } from "./Charts/AreaChart";
+export { default as LineChart } from "./Charts/LineChart";
+export { default as StackedChart } from "./Charts/StackedChart";
+export { default as ChartsHeader } from "./ChartsHeader";
+export { default as Header } from "./Header";
