944dbd2863727b15c42971fb2808da5714de9e72
aryachirag11 <aryachirag11@gmail.com>
2024-07-23 09:11:21 +0530

    Add data tables for Orders Page, Employees Page and Customers Page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Edit,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
+import Header from "./../components/Header";
+import { employeesData, employeesGrid } from "./../data/dummy";
 
 const Employees = () => {
-  return <div>Employee</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        toolbar={["Search"]}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Edit, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Employees;
